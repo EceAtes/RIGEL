2721244787f375955328726440c95bc024412c39
Ece Ateş <eceates625@gmail.com>
2023-05-13 18:58:12 +0300

    PDF Generation (Contents of the PDF are hardcoded)

---
+++ src/main/java/com/example/rigel_v1/controllers/PDFController.java
@@ -0,0 +1,33 @@
+package com.example.rigel_v1.controllers;
+
+import com.example.rigel_v1.service.PDFService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class PDFController {
+    private final PDFService pdfService;
+
+    public PDFController(PDFService pdfService) {
+        this.pdfService = pdfService;
+    }
+
+    @GetMapping("/pdf/generate")
+    public void generatePDF(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        this.pdfService.export(response);
+    }
+}
---
+++ src/main/java/com/example/rigel_v1/service/PDFService.java
@@ -0,0 +1,33 @@
+package com.example.rigel_v1.service;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class PDFService {
+    public void export(HttpServletResponse response) throws IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+        Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        fontTitle.setSize(18);
+
+        Paragraph paragraph = new Paragraph("RIGEL", fontTitle);
+        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+
+        Font fontParagraph = FontFactory.getFont(FontFactory.HELVETICA);
+        fontParagraph.setSize(12);
+
+        Paragraph paragraph2 = new Paragraph("PDF Generated UwU.", fontParagraph);
+        paragraph2.setAlignment(Paragraph.ALIGN_LEFT);
+
+        document.add(paragraph);
+        document.add(paragraph2);
+        document.close();
+    }
+}
