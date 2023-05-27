05fb483400269b984520ddda63576add2ffce355
Ece Ateş <eceates625@gmail.com>
2023-05-27 16:45:02 +0300

    Secretary Controller created

---
+++ src/main/java/com/example/rigel_v1/controllers/SecretaryController.java
@@ -0,0 +1,9 @@
+package com.example.rigel_v1.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/secretary")
+@RestController
+public class SecretaryController {
+}
