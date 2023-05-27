a0a19832a8979a257e2ed1a002351b03af0382c7
Ece Ateş <eceates625@gmail.com>
2023-05-27 09:35:06 +0300

    Insrtuctor/Student course id's are returned with JSON (not in the preferred style of JSON)

--- src/main/java/com/example/rigel_v1/domain/StudentCourse.java
+++ src/main/java/com/example/rigel_v1/domain/StudentCourse.java
@@ -1,6 +1,7 @@
 package com.example.rigel_v1.domain;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,55 +14,61 @@ import lombok.*;
 
 
 @Entity
-@Setter @Getter @NoArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
 public class StudentCourse {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;            
-    
+    private Long id;
+
     @JsonProperty("name")
     private CourseName courseName;
 
-  //  @JsonProperty("student")
+    //  @JsonProperty("student")
     @ManyToOne
     @JoinColumn(name = "student_id")
     @JsonBackReference
     private Student courseTaker;
 
     @ManyToOne
+    @JsonBackReference
     private Instructor instructor;
 
     @OneToMany
     @JoinColumn(name = "course_id")
-    private List<InternshipReport> internshipReports;
+    @JsonBackReference
+    private List<InternshipReport> internshipReports = new ArrayList<>();
 
 
-    private String internshipReportFolderID;
-    
+    private String internshipReportFolderID = "";
+
     private int iterationCount = 0;
-    
+
     @OneToOne
+    @JsonBackReference
     private CriteriaReport criteriaReport;
 
     @OneToMany
     @JoinColumn(name = "course_id")
-    private List<GradeForm> gradeForms;
- 
-    // @JsonProperty("score")
-    private Score score;
+    @JsonBackReference
+    private List<GradeForm> gradeForms = new ArrayList<>();
+
+    @JsonProperty("score")
+    private Score score = Score.not_determined;
 
     @JsonProperty("status")
-    private Status status;
+    private Status status = Status.waitingSummerTrainingEvaluationFromCompany;
 
     //private Date internshipReportDeadline;
 
 
-    public StudentCourse(Student courseTaker, CourseName courseName){   
+    public StudentCourse(Student courseTaker, CourseName courseName) {
         this.courseTaker = courseTaker;
         this.courseName = courseName;
         this.instructor = null;
-        internshipReports = new LinkedList<>();   
+        internshipReports = new LinkedList<>();
         internshipReportFolderID = "none";
         iterationCount = 0;
         //criteriaReport = new CriteriaReport();
@@ -74,11 +81,11 @@ public class StudentCourse {
         //courseTaker.enrollCourse(this);
     }
 
-    public StudentCourse(Student courseTaker, CourseName courseName, Instructor instructor){      
+    public StudentCourse(Student courseTaker, CourseName courseName, Instructor instructor) {
         this.courseTaker = courseTaker;
-        this.courseName = courseName;  
+        this.courseName = courseName;
         this.instructor = instructor;
-        internshipReports = new LinkedList<>();   
+        internshipReports = new LinkedList<>();
         internshipReportFolderID = "none";
         iterationCount = 0;
         criteriaReport = new CriteriaReport();
@@ -89,50 +96,52 @@ public class StudentCourse {
         //instructor.addCourse(this);  
     }
 
-    public void uploadInternshipReport(InternshipReport rep){
+    public void uploadInternshipReport(InternshipReport rep) {
         internshipReports.add(rep);
         System.out.println("internship report of " + courseTaker.getName() + " && " + courseName + " uploaded");
     }
-    
-    public InternshipReport getLastInternshipReportSubmitted(){
+
+    public InternshipReport getLastInternshipReportSubmitted() {
         return internshipReports.get(0);        // last iteration
     }
-    
-    public boolean generateGradeForm(){ 
-        if(instructor.getESignature() != null){
-            if(gradeForms.get(0).isWillBeRevised() 
-               || (criteriaReport.getReportStatus() == ReportStatus.submitted)&&  gradeForms.get(0).getStudentScore() != -1 )
-                    System.out.println("You can create pdf file");
-                    // create pdf!!
-                    //
-                    //
-                    return true;
-        }
-        else{
+
+    public boolean generateGradeForm() {
+        if (instructor.getESignature() != null) {
+            if (gradeForms.get(0).isWillBeRevised()
+                    || (criteriaReport.getReportStatus() == ReportStatus.submitted) && gradeForms.get(0).getStudentScore() != -1)
+                System.out.println("You can create pdf file");
+            // create pdf!!
+            //
+            //
+            return true;
+        } else {
             System.out.println("Cannot create grade form -- e-signature missing");
             return false;
         }
     }
 
-    public boolean generateCriteriaReport(){ 
-        if(instructor.getESignature() != null){         // NOT NECESSEARY I GUESS?????
-            if(!gradeForms.get(0).isWillBeRevised() 
-               || (criteriaReport.getReportStatus() == ReportStatus.submitted) &&  gradeForms.get(0).getStudentScore() != -1 )
-                    System.out.println("You can create pdf file");
-                    // create pdf!!
-                    //
-                    //
-                    return true;
-        }
-        else{
+    public boolean generateCriteriaReport() {
+        if (instructor.getESignature() != null) {         // NOT NECESSEARY I GUESS?????
+            if (!gradeForms.get(0).isWillBeRevised()
+                    || (criteriaReport.getReportStatus() == ReportStatus.submitted) && gradeForms.get(0).getStudentScore() != -1)
+                System.out.println("You can create pdf file");
+            // create pdf!!
+            //
+            //
+            return true;
+        } else {
             System.out.println("Cannot create criteria report -- e-signature missing");
             return false;
         }
     }
-  
+
     public void setCourseTaker(Student courseTaker) {
         this.courseTaker = courseTaker;
         //instructor.addCourse(this);
+    }
+
+    public void uploadCriteriaReport(CriteriaReport report) {
+        this.criteriaReport = report;
     }
 
     @Override
